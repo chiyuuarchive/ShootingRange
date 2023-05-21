89694b27dfc43a02b30ea8a61080d435170f91fb
AdrianTayeh <adriantayeh@gmail.com>
2023-05-21 14:19:07 +0200

    Added pickup to weapons
    Rotations still needs fixing

---
+++ Assets/Scripts/Adrian/PickUpController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    public Gun gunScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
+    {
+        //Setup
+        if (!equipped)
+        {
+            gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+        if (equipped)
+        {
+            gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
+    }
+
+    private void Update()
+    {
+        //Check if player is in range and presses E
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
+
+        //Drop if equipped and player presses Q
+        if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        Transform playerWeaponSocket = GameObject.Find("WeaponSocket").transform;
+        transform.parent = playerWeaponSocket;
+
+
+        // Adjust weapon transform
+        //transform.position = new Vector3(playerWeaponSocket.position.x, playerWeaponSocket.position.y, playerWeaponSocket.position.z);
+        //transform.rotation = new Quaternion(playerWeaponSocket.rotation.x, playerWeaponSocket.rotation.y-90, playerWeaponSocket.rotation.z, playerWeaponSocket.rotation.w);
+
+        if(transform.gameObject.name == "Assault Rifle")
+        {
+            
+        }
+
+        else if(transform.gameObject.name == "Pistol")
+        {
+
+        }
+
+        else if(transform.gameObject.name == "SMG")
+        {
+
+        }
+
+        else if(transform.gameObject.name == "Sniper Rifle")
+        {
+
+        }
+
+        //Make rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        //Enable gun script
+        gunScript.enabled = true;
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        //Set parent to null
+        transform.SetParent(null);
+
+        //Make rigidbody not kinematic and BoxCollider normal
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        //Gun carries moment of player
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        //AddForce
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+        //Add random rotation
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random)*10);
+
+
+        //Disable gun script
+        gunScript.enabled = false;
+    }
+}
