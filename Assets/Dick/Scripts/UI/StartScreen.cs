7fe4a08cdc577bd6acfef2e42ca65e9a4669d236
chiyuuarchive <85938105+chiyuuarchive@users.noreply.github.com>
2023-05-24 18:43:00 +0200

    fixed a bug with resume button invoking start screen

--- Assets/Dick/Scripts/UI/StartScreen.cs
+++ Assets/Dick/Scripts/UI/StartScreen.cs
@@ -6,45 +6,23 @@ public class StartScreen : Screen
     [SerializeField]
     IntEventSO updateStartScreenEvent;
     [SerializeField]
-    EventSO gamePauseEvent;
-    [SerializeField]
-    EventSO gameResumeEvent;
-    [SerializeField]
     TMP_Text startScreenMsg;
 
-    private void Awake()
-    {
-
-    }
     protected override void InitiateScreen()
     {
         updateStartScreenEvent.list += UpdateScreen;
-        gamePauseEvent.list += HideScreen;
-        gameResumeEvent.list += ShowScreen;
     }
 
     protected override void OnDestroyScreen()
     {
         updateStartScreenEvent.list -= UpdateScreen;
-        gamePauseEvent.list -= HideScreen;
-        gameResumeEvent.list -= ShowScreen;
     }
 
     protected override void OnUpdateScreen(int msg)
     {
-        if (msg == -1) gameObject.SetActive(false);
+        if (msg < 0) gameObject.SetActive(false);
         else gameObject.SetActive(true);
 
         startScreenMsg.text = $"Game starting in...{msg}";
-    }
-
-    private void HideScreen()
-    {
-        gameObject.SetActive(false);
-    }
-
-    private void ShowScreen()
-    {
-        gameObject.SetActive(true);
     }
 }
